197c76b7aecdc57288f0af4fb8d3d4f666536724
seldon57 <eulerc@gmail.com>
2016-02-26 23:21:47 -0600

    Update Automated_SW_Mixing_Station.ino

--- Automated_SW_Mixing_Station.ino
+++ Automated_SW_Mixing_Station.ino
@@ -31,22 +31,8 @@ int TankSolenoidPin;
 int BoosterPumpPin;
 
 
-int mm;				// Calculated distance in mm based on the speed of sound
-
-// Define the number of samples to keep track of.  The higher the number,
-// the more the readings will be smoothed, but the slower the output will
-// respond to the input.  Using a constant rather than a normal variable lets
-// use this value to determine the size of the readings array.
-const int numReadings = 10;
-
-int readIndex = 0;              // the index of the current reading
-int readings[numReadings];     // the readings from the analog input
-int sensor_total = 0;          // the running total
-int sensor_average = 0;        // the average
-
-
 int TankHeight = 850;	// Height of the tank in mm from the bottom to the sensor
-int TankLevel = 0;			// Calculated height of the water in the tank
+int TankLevel = 0;		// Calculated height of the water in the tank
 
 time_t initialtime = 0;
 time_t currenttime = 0;
@@ -104,11 +90,7 @@ void setup() {
   pinMode(echoPin, INPUT);
   pinMode(PressureSwitchPin, INPUT);
   digitalWrite(PressureSwitchPin, HIGH);      // turn on pullup resistor
-  
-  // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
+
 }
  
 void loop(){
@@ -187,9 +169,12 @@ int PressureSwitchRead(){
 }
 
 int TankStatusRead(){
-
+  
+  int ping;
+  
+  ping = HCSR04Read(Sens1_trigPin, Sens1_echoPin);
   //Calculate the water level in the tank
-  TankLevel = TankHeight - mm;
+  TankLevel = TankHeight - ping;
   
   //14.2875 mm equals 1 gallon
 
@@ -214,12 +199,39 @@ int TankStatusRead(){
     level = LowLow;
 
   //Tank level state change if HC-SR04 is not acting properly or fails
-  if (mm > 2000)      // HC-SR04 is directly up against what it is measuring
+  if (ping > 2000)      // HC-SR04 is directly up against what it is measuring
     level = HighHigh;
-  if (mm == 0)      // HC-SR04 has failed or is disconnected
+  if (ping == 0)        // HC-SR04 has failed or is disconnected
     level = HighHigh;
     
   return level;
+}
+
+
+int HCSR04Read(int trigPin, int echoPin){
+  int i;
+  int avg = 0;
+  int mm;		    // Distance measured by the sensor in mm
+  int duration;		// Duration between trig and echo
+
+  for (i = 0; i < 5; i++){
+	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(5);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+ 	// Read the signal from the sensor: a HIGH pulse whose
+	// duration is the time (in microseconds) from the sending
+	// of the ping to the reception of its echo off of an object.
+	duration = pulseIn(echoPin, HIGH);
+ 	// convert the time into a distance
+	mm = (duration/2.00) / 2.91;
+	avg = avg + mm;    //sum sensor value
+  }
+  avg = avg / 5;    // average
+  return avg;
 }
 
 int RODIOff(){			
@@ -324,49 +336,4 @@ int RODIRunning(){
   if(currenttime >= RunTime)
     RODI = Flushing;
   return RODI;
-}
-
-int Smoothing(){
-  // subtract the last reading:
-  sensor_total = sensor_total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = HCSR04Read(Sens1_trigPin, Sens1_echoPin); // this should be the TankHeight reading rather than a raw analog input
-  // add the reading to the total:
-  sensor_total = sensor_total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
-
-  // calculate the average:
-  sensor_average = sensor_total / numReadings;
-  
-  return sensor_average;
-}
-
-int HCSR04Read(int trigPin, int echoPin){
-
-  int duration;		// Duration between trig and echo
-  int distance;		// Distance measured by the sensor in mm
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  duration = pulseIn(echoPin, HIGH);
- 
-  // convert the time into a distance
-  distance = (duration/2.00) / 2.91;
-    
-  return distance;
 }
